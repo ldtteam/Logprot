96c0fff7a0488a8bb101e2a692f466d3a6ce5f47
someaddons <someaddons@github.com>
2021-09-19 20:50:27 +0200

    version 1.4: update mods toml, add Displaytest

--- src/main/java/com/logprot/Logprot.java
+++ src/main/java/com/logprot/Logprot.java
@@ -2,9 +2,12 @@ package com.logprot;
 
 import com.logprot.config.Configuration;
 import com.logprot.event.EventHandler;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,6 +24,8 @@ public class Logprot
 
     public Logprot()
     {
+        ModLoadingContext.get()
+          .registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> org.apache.commons.lang3.tuple.Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 
         config = new Configuration();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
